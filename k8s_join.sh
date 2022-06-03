6e285285ddaf581a099e702584d6a28d0eff3caf
Code-Kartikey12 <38080424+Code-Kartikey12@users.noreply.github.com>
2022-06-03 13:36:20 +0530

    Update k8s_join.sh

--- k8s_join.sh
+++ k8s_join.sh
@@ -1,2 +1,2 @@
 kubeadm join 192.168.0.18:6443 --token ajg63i.a5znhp4fm9F8m3s9 \
-  --discovery-token-ca-cert-hashsha256:768bf679d66883781002cc182b8d68444ba41b95585d1b5ad08700e7c90ad9e
+  --discovery-token-ca-cert-hash sha256:768bf679d66883781002cc182b8d68444ba41b95585d1b5ad08700e7c90ad9e
